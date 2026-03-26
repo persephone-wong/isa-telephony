e2855f7482baaf7c1448eece9db68b5b3d1017f3
Michael <michael.lau@hotmail.ca>
2026-03-25 18:43:00 -0700

    Remove dupelicate functions

--- server/server.js
+++ server/server.js
@@ -35,7 +35,6 @@ class ServerApp {
   setupMiddleware() {
     this.app.use(express.static(this.clientDir, { index: false }));
     this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true }));
     this.app.use(cors());
     this.app.use(express.urlencoded({ extended: false }))
   }
